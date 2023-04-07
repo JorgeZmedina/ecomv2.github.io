6bac70a5d25e1bf13d63971f0735054d256b64a1
JorgeZmedina <114126984+JorgeZmedina@users.noreply.github.com>
2023-04-07 13:21:34 -0400

    created product array backup

---
+++ icoffee/src/components/ProductsArray.js
@@ -0,0 +1,89 @@
+
+const ProductsArray = [
+    {
+        "id": 1,
+        "title": "Peppermint hot chocolate",
+        "description": "Do you love hot chocolate? Do you love Peppermint? Why not have both! This drink always has brought strong emotions out of anyone that has tried it. With our new and bolder formula, this drink must be the next option for you.",
+        "price": 4,
+        "image": "https://images.unsplash.com/photo-1461023058943-07fcbe16d735?ixlib=rb-4.0.3&ixid=MnwxM[…]90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1469&q=80"
+        },
+        {
+        "id": 2,
+        "title": "Peppermint White Hot Chocolate",
+        "description": "Imagine a fireplace. The subtle crackling of the fire and the soft heat it emmits. The feeling that comes to you while you imagine this fireplace is the same feeling you will get when you try this delicious hot chocolate. For a limited time only.",
+        "price": 13,
+        "image": "https://images.unsplash.com/photo-1527156231393-7023794f363c?ixlib=rb-4.0.3&ixid=MnwxM[…]G90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=711&q=80"
+        },
+        {
+        "id": 3,
+        "title": "Toasted White Hot Chocolate",
+        "description": "A campers favorite drink. The hot chocolate is already delicious. And now toasted? Can there be a better drink than this? Only you know the answer.",
+        "price": 16,
+        "image": "https://images.unsplash.com/photo-1580661869408-55ab23f2ca6e?ixlib=rb-4.0.3&ixid=MnwxM[…]G90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=735&q=80"
+        },
+        {
+        "id": 4,
+        "title": "Hot Chocolate",
+        "description": "A timeless classic. This is a recipe passed down from generations. Its popularity stands to show that hot chocolate is always the answer.",
+        "price": 9,
+        "image": "https://images.unsplash.com/photo-1557006021-b85faa2bc5e2?ixlib=rb-4.0.3&ixid=MnwxMjA3[…]G90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80"
+        },
+        {
+        "id": 5,
+        "title": "Strawberry Crème Frappuccino Blended Beverage",
+        "description": "Look at the beauty of this drink. The answer is yes. It tastes as good as it looks.",
+        "price": 12,
+        "image": "https://images.unsplash.com/photo-1604423043492-41303788de80?ixlib=rb-4.0.3&ixid=MnwxM[…]G90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=699&q=80"
+        },
+        {
+        "id": 6,
+        "title": "Peppermint White Chocolate Mocha Frappuccino Blended Beverage",
+        "description": "This drink was created by an employee. Our mission statement is to ensure that you to have the best experience possible. With our Peppermint White Chocolate Mocha Frappuccino Blended Beverage, our staff is proving this mission statement.",
+        "price": 5,
+        "image": "https://images.unsplash.com/photo-1481455473976-c280ae7c10f9?ixlib=rb-4.0.3&ixid=MnwxM[…]90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1471&q=80"
+        },
+        {
+        "id": 7,
+        "title": "Pistachio Frappuccino Blended Beverage",
+        "description": "Who says coffee can't be healthy. Well this drink proves them wrong. It has subtle hints of pistachio, 50% less sugar, and losses zero flavor. On sale now",
+        "price": 4,
+        "image": "https://images.unsplash.com/photo-1512484149279-9751676e56c4?ixlib=rb-4.0.3&ixid=MnwxM[…]90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80"
+        },
+        {
+        "id": 8,
+        "title": "Mocha Cookie Crumble Frappuccino",
+        "description": "The drink your kids always want. But you tell them no because it has too much sugar. Well don't think about your kids. Think about you. A little extra sugar never hurt anyone. Plus, your parents are not around to say no.",
+        "price": 7,
+        "image": "https://images.unsplash.com/photo-1461010083959-8a5727311252?ixlib=rb-4.0.3&ixid=MnwxM[…]90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1424&q=80"
+        },
+        {
+        "id": 9,
+        "title": "Dragon Drink",
+        "description": "Truly the best blend of fruits you will ever try. This recipe was inspired from the movie \"How to train your dragon.\"",
+        "price": 11,
+        "image": "https://images.unsplash.com/photo-1471922597728-92f81bfe2445?ixlib=rb-4.0.3&ixid=MnwxM[…]90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1472&q=80"
+        },
+        {
+        "id": 10,
+        "title": "Mango Dragon Fruit",
+        "description": "Mango is such a delicous fruit. It is underestimated in most mainstream drinks. This drink is here to turn the tides and give mangoes a nice reputation.",
+        "price": 3,
+        "image": "https://images.unsplash.com/photo-1473115209096-e0375dd6b3b3?ixlib=rb-4.0.3&ixid=MnwxM[…]90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80"
+        },
+        {
+        "id": 11,
+        "title": "Paradise Drink",
+        "description": "We know your job is stressful. Unfortunately you do not have enough saved for a full vacation. The paradise drink is just for you. Enjoy a delicious drink that tastes like a miny paradise every day before work. It's the small things that matter.",
+        "price": 5,
+        "image": "https://images.unsplash.com/photo-1619158401201-8fa932695178?ixlib=rb-4.0.3&ixid=MnwxM[…]G90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80"
+        },
+        {
+        "id": 12,
+        "title": "Strawberry Açaí",
+        "description": "We could hide this drink in our secret menu and it would still be found and promoted on social media. I can't lie it's my favorite drink too. You might as well get it now before it goes on back-order again.",
+        "price": 9,
+        "image": "https://images.unsplash.com/photo-1553530666-ba11a7da3888?ixlib=rb-4.0.3&ixid=MnwxMjA3[…]G90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=686&q=80"
+        }
+];
+
+export default ProductsArray;
